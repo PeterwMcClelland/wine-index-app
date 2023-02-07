7fdc817af27c28e2b441c3db740b4585c0223278
Peter McClelland <peterwmcclelland@gmail.com>
2023-02-06 19:08:45 -0600

    header navbar

--- wine-list-app/src/components/Header.js
+++ wine-list-app/src/components/Header.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import { AppBar, Typography } from '@mui/material';
+import React, { useState } from 'react'
+import { AppBar, Tab, Tabs, Toolbar, Typography } from '@mui/material';
+import WineBarIcon from '@mui/icons-material/WineBar';
 
 
 export const Header = () => {
+    const [value, setValue] = useState();
   return (
     <div>
-        <AppBar position='sticky'>
-            <Typography>Hello World</Typography>
+        <AppBar position="sticky">
+            
+        <Toolbar>
+            <Typography>
+                <WineBarIcon />
+            </Typography>
+            <Tabs 
+            sx={{ml:'auto'}}
+            textColor='inherit' 
+            indicatorColor='secondary' 
+            value={value} 
+            onChange={( e, val) => setValue(val)}
+            >
+                <Tab label='Add Wine'/>
+                    <Tab label='All Wines'/>
+                    <Tab label='About Us'/>
+            </Tabs>
+        </Toolbar>
         </AppBar>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
